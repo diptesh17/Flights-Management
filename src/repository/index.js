bea7212be41d768664b47fed9ee841a4761ffc51
diptesh17 <deorediptesh1883@gmail.com>
2024-01-19 09:58:43 +0530

    Added City Service

---
+++ src/repository/index.js
@@ -0,0 +1,5 @@
+// safer way to export all repositories
+
+module.exports = {
+  CityRepository: require("./city-repository"),
+};
---
+++ src/services/city-service.js
@@ -0,0 +1,47 @@
+const { CityRepository } = require("../repository/index");
+
+class cityService {
+  constructor() {
+    this.cityRepository = new CityRepository();
+  }
+
+  async createCity(data) {
+    try {
+      const city = await this.cityRepository.createCity(data);
+      return city;
+    } catch (error) {
+      console.log("Somwthing went wrong at service layer");
+      throw { error };
+    }
+  }
+
+  async updateCity(cityId, data) {
+    try {
+      const city = await this.cityRepository.updateCity(cityId, data);
+      return city;
+    } catch (error) {
+      console.log("Somwthing went wrong at service layer");
+      throw { error };
+    }
+  }
+
+  async deleteCity(cityId) {
+    try {
+      const response = await this.cityRepository.deleteCity(cityId);
+      return response;
+    } catch (error) {
+      console.log("Somwthing went wrong at service layer");
+      throw { error };
+    }
+  }
+
+  async getCity(cityId) {
+    try {
+      const city = await this.cityRepository.getCity(cityId);
+      return city;
+    } catch (error) {
+      console.log("Somwthing went wrong at service layer");
+      throw { error };
+    }
+  }
+}
