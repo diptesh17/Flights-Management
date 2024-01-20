36f8461a5712173e8149c74a0b4410ce8d084172
diptesh17 <deorediptesh1883@gmail.com>
2024-01-20 07:25:28 +0530

    Minor Fixes

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -37,12 +37,18 @@ class CityRepository {
 
   async updateCity(cityId, data) {
     try {
-      const city = await City.update(data, {
-        where: {
-          id: cityId,
-        },
-      });
+      // This approach works , but didn't return updated object
+      // for postgress , returning:true is use  , else not
 
+      // const city = await City.update(data, {
+      //   where: {
+      //     id: cityId,
+      //   },
+      // });
+
+      const city = await City.findByPk(cityId);
+      city.name = data.name;
+      await city.save();
       return city;
     } catch (error) {
       console.log("Somwthing went wrong in repository layer");
